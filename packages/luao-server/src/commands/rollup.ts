04b5af83638412933a3d7603d4bd0b9eb9af58b7
liudaoyun <liudaoyun@xforceplus.com>
2024-01-15 18:32:58 +0800

    fix(:bug:): luao-server

--- packages/luao-server/src/commands/rollup.ts
+++ packages/luao-server/src/commands/rollup.ts
@@ -1,2 +1,14 @@
-import { buildReact } from 'luao-rollup'
-buildReact()
+import { buildReact } from 'luao-rollup';
+import { Command } from 'commander'
+const program = new Command();
+const handleFileCmd = () => { 
+    program
+    .option('-w,--w','watch code running...')
+    .action((name) => {
+        buildReact(name)
+    }
+).parse(process.argv)
+return Promise.resolve('done')
+}
+
+handleFileCmd();
