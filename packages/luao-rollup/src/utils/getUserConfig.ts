210e4b9c44dbe05229de2fa041027666a24e1cfd
liudaoyun <scottldy0316@gmail.com>
2024-09-20 16:02:13 +0800

    Update getUserConfig.ts
    修复判断系统加载文件错误

--- packages/luao-rollup/src/utils/getUserConfig.ts
+++ packages/luao-rollup/src/utils/getUserConfig.ts
@@ -1,13 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import { IBundleOptions } from '../types';
+import os from 'os'
 
 export default async function (): Promise<Array<IBundleOptions> | IBundleOptions>  {
   const userConfigName = 'luao.rollup-config.js';
   const userConfigPath = path.resolve(userConfigName);
 
   if (fs.existsSync(userConfigPath)) {
-    const userConfigFn = await import(userConfigPath);
+    let platform = os.platform()
+    const userConfigFn = await import(platform === 'win32'?`file://${userConfigPath}`:userConfigPath);
     // TODO 校验扩展项是否配置正确
     if (typeof userConfigFn === 'object') {
       return userConfigFn.default;
